0f4fa65871a12e1c92399cee18361d043a7551a8
travis_gandy <travis_gandy@yahoo.com>
2021-04-07 19:16:31 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1 = 2
+var num2 = 31
+let multiply = (num1 * num2);
+
+var random = Math.round((Math.random() * 10) + 1);
+
+var num3 = 24
+var num4 = 5
+let mod = (num3 % num4);
+
+var max = Math.max(1, 2, 3, 4, 5, 10, 15, 20);
